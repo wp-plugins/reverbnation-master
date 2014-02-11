43371c09dfa6da8908a851afe12730a4cd804f79
TechGasp <TechGasp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-11 22:08:32 +0000

    version 4.3
    git-svn-id: https://plugins.svn.wordpress.org/reverbnation-master/trunk@855905 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: Techgasp
 Tags: wordpress, plugin, widget, sound, music, mp3, wav, player, playlist, embedded, reverbnation, reverb nation
 Requires at least: 3.5
-Tested up to: 3.8
-Stable tag: 4.1
+Tested up to: 3.8.1
+Stable tag: 4.3
 License: GPLv2 or later
 
 Reverbnation Master plugs-in perfectly into wordpress and allows you to display all the reverbnation juice inside any widget template position.
@@ -16,21 +16,41 @@ Reverbnation Master plugin allows you to display all the reverbnation juicy widg
 
 With fast loading speeds, Reverbnation Master as a very small system trace and is based in reverbnation html5 api. Check the screenshots page or get in touch with us in case of doubts.
 
+Both the widget and the shortcode framework are fully mobile responsive, a must for any professional website.
+
 IMPORTANT: Errors and conflicts free. NO JAVASCRIPT or AJAX.
 
 **independent music for wordpress**
 
 **LITE Version Contains:**
 
-* Display or hide Widget Title
-* Cool transparent Reverbnation Profile Button
+* Display, Hide or Change any Widget Title
+* Reverbnation Master Buttons Widget:
+* Cool transparent Reverbnation Profile Button to connect your wordpress and boost up your Reverbnation Profile
+* Fully Responsive
 
 **ADVANCED Version Contains:**
 
-* Display or hide Widget Title
-* Cool transparent Reverbnation Profile Button
-* Reverbnation Widget Player (full nine yards api Player. Displays all Reverbnation Widgets, Html5 Player, Html5 Fan Collector, Html5 Show Schedule, Tune Widget, Shop, etc.)
-* Shortcode Framework v2. Publish widget inside pages and posts
+* Fully Mobile Responsive
+* Display, Hide or Change any Widget Title
+* Reverbnation Master Buttons Widget:
+* Cool transparent Reverbnation Profile Button to connect your wordpress and boost up your Reverbnation Profile
+
+Reverbnation Master Advanced Responsive Widget:
+* Makes your life easy to manage all Reverbnation Widgets
+* Cleans and speeds up Reverbnation scripts for optimal page load times
+* Improved Google SEO, encapsulates all Reverbnation scripts in a nice tidy google way
+* Reverbnation Advanced Responsive Player, full nine yards api Player
+* Displays all Reverbnation Widgets
+* Html5 Player
+* Html5 Fan Collector
+* Html5 Show Schedule
+* Tune Widget
+* Shop
+* etc.
+
+* Shortcode Framework v3. Universal Shortcode for all pages and posts and Individual Shortcode for each page or post
+* Shortcodes allow you to professionally display Reverbnation sripts inside your wordpress pages and posts
 * [Advanced Version Link](http://wordpress.techgasp.com/reverbnation-master/)
 
 **ONLINE DEMO**
@@ -49,9 +69,9 @@ IMPORTANT: Errors and conflicts free. NO JAVASCRIPT or AJAX.
 
 == Installation ==
 
-1. Use the Wordpress Installer, Updater.
-2. Activate in the "Plugins" admin panel.
-3. Visit the "Widget Page | Reverbnation Master" to configure the settings and your template widget position.
+1. Use the Wordpress Installer, Updater
+2. Activate in the "Plugins" admin panel
+3. Visit the "Widget Page | Reverbnation Master Widgets" to configure widgets settings and your template widget positions
 4. [Reverbnation Master Documentation](http://wordpress.techgasp.com/reverbnation-master-documentation)
 
 
@@ -60,20 +80,21 @@ IMPORTANT: Errors and conflicts free. NO JAVASCRIPT or AJAX.
 = Issues with buttons display =
 * Please update to latest version. Up-to-date API's are available
 
-= Where is the player? =
-* Advanced Version contains Reverbnation Profile Button plus the Reverbnation Widget Player.
+= Where is the player and all widgets? =
+* Advanced Version Add-ons contains all widgets
 * [Advanced Version Link](http://wordpress.techgasp.com/reverbnation-master/)
 
-= Where is the shortcode? =
-* Advanced Version contains the shortcode framework v2.
-* Insert the shortcode into any page or post. [reverbnation-master]
+= Insert Shortcode in Pages or Posts =
+* Advanced Version Add-ons contain the shortcode framework v3
+* Insert the universal shortcode into any page or post. [reverbnation-master-un]
+* Insert the individual shortcode into any page or post. [reverbnation-master]
 * [Advanced Version Link](http://wordpress.techgasp.com/reverbnation-master/)
 
-= What's this Shortcode Framework v2 =
-* It's a framework developed by TechGasp and built upon wordpress marvellous resources.
-* Allows users to quickly and easily display plugins or widgets inside their pages and posts.
-* It's available in all TechGasp advanced version plugins. Lite versions plugins do not include shortcode framework v2.
-* [About Shortcode Framework v2](http://wordpress.techgasp.com/new-wordpress-framework-from-techgasp/)
+= What's this Shortcode Framework v3 =
+* It's a framework developed by TechGasp and built upon wordpress marvellous resources
+* Allows users to quickly and easily display plugins or widgets inside their pages and posts
+* It's available in all TechGasp advanced version plugins. Lite versions plugins do not include shortcode framework v3
+* [About Shortcode Framework v3](http://wordpress.techgasp.com/techgasp-wordpress-framework-v3/)
 
 = Need More Help =
 * [Reverbnation Master Documentation](http://wordpress.techgasp.com/reverbnation-master-documentation)
@@ -82,16 +103,26 @@ IMPORTANT: Errors and conflicts free. NO JAVASCRIPT or AJAX.
 
 == Screenshots ==
 
-1. Widget With Reverbnation Profile Button + Reverbnation Html5 Shop
-2. Widget With Reverbnation Profile Button + Reverbnation Html5 Player Horizontal
-3. Widget With Reverbnation Profile Button + Reverbnation Html5 Show Schedule
-4. Widget With Reverbnation Profile Button + Reverbnation Html5 Player Vertical
-5. Widget With Reverbnation Profile Button + Reverbnation Html5 Player Horizontal
-6. Widget With Reverbnation Profile Button + Reverbnation Html5 Newsletter
-7. Widget inside pages and posts. Shortcode framework implementations. Reverbnation Player inside a page.
+1. Reverbnation Master Advanced Responsive Widget, Html5 Shop + Reverbnation Master Buttons Widget
+2. Reverbnation Master Advanced Responsive Widget, Html5 Player Horizontal
+3. Reverbnation Master Advanced Responsive Widget, Html5 Show Schedule
+4. Reverbnation Master Advanced Responsive Widget, Html5 Player Vertical
+5. Reverbnation Master Advanced Responsive Widget, Html5 Player Horizontal
+6. Reverbnation Master Advanced Responsive Widget, Html5 Newsletter
+7. Shortcode inside pages and posts. Shortcode framework implementations. Reverbnation Player inside a page.
 
 
 == Changelog ==
+
+= 4.3 =
+* New TechGasp Plugin Framework
+* New Plugin Menu
+* New Administrator Page
+* New Universal Shortcode that can be combined with Individual Shortcodes in Pages and Posts
+* Adding index SEC
+* New Reverbnation API
+* New Reverbnation Master Buttons Widget
+* New Reverbnation Master Advanced Responsive Widget
 
 = 4.1 =
 * Wordpress 3.8 Ready
@@ -130,8 +161,11 @@ IMPORTANT: Errors and conflicts free. NO JAVASCRIPT or AJAX.
 
 == Upgrade Notice ==
 
+= 4.3 =
+* IMPORTANT Upgrade to new TechGasp Plugin Framework v3
+
 = 4.0 =
-* IMPORTANT Upgrade to new TechGasp Plugin Framework
+* IMPORTANT Upgrade to new TechGasp Plugin Framework v2
 
 = 2.0 =
 * IMPORTANT Upgrade to Stable release
--- reverbnation-master.php
+++ reverbnation-master.php
@@ -2,117 +2,113 @@
 /**
 Plugin Name: Reverbnation Master
 Plugin URI: http://wordpress.techgasp.com/reverbnation-master/
-Version: 4.1
+Version: 4.3
 Author: TechGasp
 Author URI: http://wordpress.techgasp.com
 Text Domain: reverbnation-master
 Description: Reverbnation Master plugs-in perfectly into wordpress and allows you to display all the reverbnation juice inside any widget template position.
 License: GPL2 or later
 */
-/*  Copyright 2013 TechGasp  (email : info@techgasp.com)
+/*
+Copyright 2013 TechGasp  (email : info@techgasp.com)
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-
 if(!class_exists('reverbnation_master')) :
-
-// DEFINE PLUGIN ID
+///////DEFINE ID//////
 define('REVERBNATION_MASTER_ID', 'reverbnation-master');
+///////DEFINE VERSION///////
+define( 'reverbnation_master_VERSION', '4.3' );
+global $reverbnation_master_version, $reverbnation_master_name;
+$reverbnation_master_version = "4.3"; //for other pages
+$reverbnation_master_name = "Reverbnation Master"; //pretty name
+if( is_multisite() ) {
+update_site_option( 'reverbnation_master_installed_version', $reverbnation_master_version );
+update_site_option( 'reverbnation_master_name', $reverbnation_master_name );
+}
+else{
+update_option( 'reverbnation_master_installed_version', $reverbnation_master_version );
+update_option( 'reverbnation_master_name', $reverbnation_master_name );
+}
+// HOOK ADMIN
+require_once( dirname( __FILE__ ) . '/includes/reverbnation-master-admin.php');
+// HOOK ADMIN IN & UN SHORTCODE
+require_once( dirname( __FILE__ ) . '/includes/reverbnation-master-admin-shortcodes.php');
+// HOOK ADMIN WIDGETS
+require_once( dirname( __FILE__ ) . '/includes/reverbnation-master-admin-widgets.php');
+// HOOK ADMIN ADDONS
+require_once( dirname( __FILE__ ) . '/includes/reverbnation-master-admin-addons.php');
+// HOOK ADMIN UPDATER
+require_once( dirname( __FILE__ ) . '/includes/reverbnation-master-admin-updater.php');
+// HOOK WIDGET BUTTONS
+require_once( dirname( __FILE__ ) . '/includes/reverbnation-master-widget-buttons.php');
 
-// DEFINE PLUGIN NICK
-define('REVERBNATION_MASTER_NICK', 'Reverbnation Master');
 
-// HOOK WIDGET
-require_once( dirname( __FILE__ ) . '/includes/reverbnation-master-widget.php');
-
-// HOOK INVITATION
-
-// HOOK SHORTCODE
-
-	class reverbnation_master{
-		/** function/method
-		* Usage: hooking the plugin options/settings
-		* Arg(0): null
-		* Return: void
-		*/
-		public static function reverbnation_master_register()
-		{
-			register_setting(REVERBNATION_MASTER_ID, 'tsm_quote');
-		}
-		/** function/method
-		* Usage: hooking (registering) the plugin menu
-		* Arg(0): null
-		* Return: void
-		*/
-		public static function menu()
-		{
-			// Create menu tab
-			add_options_page(REVERBNATION_MASTER_NICK.' Plugin Options', REVERBNATION_MASTER_NICK, 'manage_options', REVERBNATION_MASTER_ID.'-admin', array('reverbnation_master', 'options_page'));
-			add_filter( 'plugin_action_links', array('reverbnation_master', 'reverbnation_master_link'), 10, 2 );
-		}
-		/** function/method
-		* Usage: show options/settings form page
-		* Arg(0): null
-		* Return: void
-		*/
-		public static function options_page()
-		{
-			if (!current_user_can('manage_options'))
-			{
-				wp_die( __('You do not have sufficient permissions to access this page.') );
-			}
-			$plugin_id = REVERBNATION_MASTER_ID;
-			// display options page
-			include( dirname( __FILE__ ) . '/includes/reverbnation-master-admin.php');
-		}
-		/** function/method
-		* Usage: show options/settings form page
-		* Arg(0): null
-		* Return: void
-		*/
-		 public static function reverbnation_master_widget()
-		{
-			// display widget page
-			include( dirname( __FILE__ ) . '/includes/reverbnation-master-widget.php');
-		}
-		/** function/method
-		* Usage: filtering the content
-		* Arg(1): string
-		* Return: string
-		*/
-		public static function content_with_quote($content)
-		{
-			$quote = '<p>' . get_option('tsm_quote') . '</p>';
-			return $content . $quote;
-		}
-		// Add settings link on plugin page
-		public static function reverbnation_master_link($links, $file) {
-			static $this_plugin;
-			if (!$this_plugin) $this_plugin = plugin_basename(__FILE__);
-			if ($file == $this_plugin){
-				$settings_link = '<a href="' . admin_url( 'options-general.php?page='.REVERBNATION_MASTER_ID).'-admin' . '">' . __( 'Settings' ) . '</a>';
-				array_unshift($links, $settings_link);
-			}
-		return $links;
-		}
-		// Advanced Updater
+class reverbnation_master{
+//REGISTER PLUGIN
+public static function reverbnation_master_register(){
+register_setting(REVERBNATION_MASTER_ID, 'tsm_quote');
+}
+public static function content_with_quote($content){
+$quote = '<p>' . get_option('tsm_quote') . '</p>';
+	return $content . $quote;
+}
+//SETTINGS LINK IN PLUGIN MANAGER
+public static function reverbnation_master_links( $links, $file ) {
+	if ( $file == plugin_basename( dirname(__FILE__).'/reverbnation-master.php' ) ) {
+		$links[] = '<a href="' . admin_url( 'admin.php?page=reverbnation-master' ) . '">'.__( 'Settings' ).'</a>';
 	}
-	if ( is_admin() )
-		{
-		add_action('admin_init', array('reverbnation_master', 'reverbnation_master_register'));
-		add_action('admin_menu', array('reverbnation_master', 'menu'));
-		}
-	add_filter('the_content', array('reverbnation_master', 'content_with_quote'));
+
+	return $links;
+}
+
+public static function reverbnation_master_updater_version_check(){
+global $reverbnation_master_version;
+//CHECK NEW VERSION
+$reverbnation_master_slug = basename(dirname(__FILE__));
+$current = get_site_transient( 'update_plugins' );
+$reverbnation_plugin_slug = $reverbnation_master_slug.'/'.$reverbnation_master_slug.'.php';
+@$r = $current->response[ $reverbnation_plugin_slug ];
+if (empty($r)){
+$r = false;
+$reverbnation_plugin_slug = false;
+if( is_multisite() ) {
+update_site_option( 'reverbnation_master_newest_version', $reverbnation_master_version );
+}
+else{
+update_option( 'reverbnation_master_newest_version', $reverbnation_master_version );
+}
+}
+if (!empty($r)){
+$reverbnation_plugin_slug = $reverbnation_master_slug.'/'.$reverbnation_master_slug.'.php';
+@$r = $current->response[ $reverbnation_plugin_slug ];
+if( is_multisite() ) {
+update_site_option( 'reverbnation_master_newest_version', $r->new_version );
+}
+else{
+update_option( 'reverbnation_master_newest_version', $r->new_version );
+}
+}
+}
+		// Advanced Updater
+
+//END CLASS
+}
+if ( is_admin() ){
+	add_action('admin_init', array('reverbnation_master', 'reverbnation_master_register'));
+	add_action('init', array('reverbnation_master', 'reverbnation_master_updater_version_check'));
+}
+add_filter('the_content', array('reverbnation_master', 'content_with_quote'));
+add_filter( 'plugin_action_links', array('reverbnation_master', 'reverbnation_master_links'), 10, 2 );
 endif;
-?>
